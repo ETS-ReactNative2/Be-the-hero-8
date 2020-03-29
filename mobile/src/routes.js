ea9ddde3eb19949a6f5b3dde76016cb2d34a0e5b
bruno <brunosilvcarv@gmail.com>
2020-03-29 18:42:24 -0400

    tela inicial mobile

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 
 const AppStack = createStackNavigator();
 
-import Incidents from './pages/Incidents';
-import Detail from './pages/Detail';
+import Incidents from "./pages/Incidents";
+import Detail from "./pages/Detail";
 
 export default function Routes() {
-    return (
+    return(
         <NavigationContainer>
-            <AppStack.Navigator>
-                <AppStack.Screen component={Incidents} />
-                <AppStack.Screen component={Detail} />
+
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
+                <AppStack.Screen name="Incidents" component={Incidents}/>
+                <AppStack.Screen name="Detail" component={Detail} />
             </AppStack.Navigator>
+
         </NavigationContainer>
     );
 }
